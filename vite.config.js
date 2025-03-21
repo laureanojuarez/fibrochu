991c85256c2038aaf85ad3c3b3c702c9ac4168db
Laureano <laureanojuarez69@gmail.com>
2025-03-21 02:43:45 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: process.env.VITE_BASE_PATH || "/fibrochu",
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 });
